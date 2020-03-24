4f1fe9cf63710a17183e3367d835b50ab32a7170
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2020-03-24 15:43:42 -0500

    delete n read tapes method

--- TuringLab/src/model/TuringMachine.java
+++ TuringLab/src/model/TuringMachine.java
@@ -67,4 +67,75 @@ public class TuringMachine {
 			}
 		}
 	}
+
+	public void deleteTape(int head) {
+		if(n==1) {
+			c0=null;
+			c1=null;
+			c2=null;
+			n--;
+		}
+		if(c0!=null) {
+			switch(head) {
+			case 0:
+				c0=c0.getNextTape();
+				c0.getNextTape().setPrevTape(null);
+				n--;
+				if(n%2 !=0) {
+					c1=c1.getNextTape();
+				}
+				break;
+			case 1:
+				if(n%2 ==0) {
+					Tape next = c1.getNextTape();
+					c1.getPrevTape().setNextTape(next);
+					next.setPrevTape(c1.getPrevTape());
+					c1=next;
+					n--;
+				}else {
+					c1.getPrevTape().setNextTape(c1.getNextTape());
+					c1.getNextTape().setPrevTape(c1.getPrevTape());
+					c1=c1.getPrevTape();
+					n--;
+				}
+				break;
+			case 2:
+				c2.getPrevTape().setNextTape(null);
+				c2=c2.getPrevTape();
+				n--;
+				if(n%2 ==0) {
+					c1=c1.getPrevTape();
+				}
+				break;
+			}
+		}
+	}
+	
+	public char readTape(int head) {
+		char tape='0';
+		switch(head) {
+		case 0:
+			if(c0!=null) {
+				tape=c0.getLetter();
+			}else {
+				tape='#';
+			}
+			break;
+		case 1:
+			if(c1!=null) {
+				tape=c1.getLetter();
+			}else {
+				tape='#';
+			}
+			break;
+		case 2:
+			if(c2!=null) {
+				tape=c2.getLetter();
+			}else {
+				tape='#';
+			}
+			break;
+		}
+		return tape;
+	}
 }
